5d20b36867bb2d8be808a1477a4f7aaa221e0a57
n23025 <n23025@std.it-college.ac.jp>
2023-06-30 04:29:13 +0000

    5

---
+++ hakkason/poniitail.py
@@ -0,0 +1,16 @@
+def eye_exam():
+    correct_answers = 0
+
+    for _ in range(5):
+        d, e = input().split()
+
+        if d == e:
+            correct_answers += 1
+
+    if correct_answers >= 3:
+        print("OK")
+    else:
+        print("NG")
+
+
+eye_exam()
